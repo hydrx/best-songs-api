53d6ccdcf85c857634984bbcae9ada09da3e4468
Ticia Francisco <ticiafrancisco@gmail.com>
2022-06-24 14:44:00 -0700

    Add bands data to server.js

--- server.js
+++ server.js
@@ -4,9 +4,64 @@ const PORT = 8000
 const cors = require("cors")
 
 app.use(cors())
+app.use("static", express.static("./static/"))
 
 const bands = {
-
+    "unknown":
+    {
+        "song": "no data",
+        "youtube": "#"
+    },
+    "Dance Gavin Dance":
+	{
+		"song": "Alex English",
+		"youtube": "#"
+	},
+	"Paramore":
+	{
+		"song": "Pressure",
+		"youtube": "#"
+	},
+	"Panic! At The Disco":
+	{
+		"song": "Lying Is The Most Fun A Girl Can Have Without Taking Her Clothes Off",
+		"youtube": "#"
+	},
+	"Fall Out Boy":
+	{
+		"song": "Grand Theft Autumn",
+		"youtube": "#"
+	},
+	"My Chemical Romance":
+	{
+		"song": "I'm Not Okay (I Promise)",
+		"youtube": "#"
+	},
+	"Hawthorne Heights":
+	{
+		"song": "Ohio Is For Lovers",
+		"youtube": "#"
+	},
+	"Escape The Fate":
+	{
+		"song": "This War Is Ours (The Guillotine II)",
+		"youtube": "#"
+	},
+	"Mayday Parade":
+	{
+		"song": "Three Cheers For Five Years",
+		"youtube": "#"
+	},
+	"Jimmy Eat World":
+	{
+		"song": "Hear You Me",
+		"youtube": "#"
+	},
+	"Bright Eyes":
+	{
+		"song": "Lover I Don't Have To Love",
+		"youtube": "#"
+	}
 }
 
 app.get('/', (req,res) =>{
