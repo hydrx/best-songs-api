30912e9b96601b16cb09ce05827ed60fd3b5763f
Ticia Francisco <ticiafrancisco@gmail.com>
2022-06-24 15:35:59 -0700

    Update main.js for working front-end

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,8 @@ document.querySelector("button").addEventListener("click", getBestSong)
 async function getBestSong(){
     const bandName = document.querySelector("input").value
     try{
-        const res = await fetch(`localhost:8000/api/${bandName}`)
-        const data = await res.json
+        const res = await fetch(`http://localhost:8000/api/${bandName}`)
+        const data = await res.json()
 
         console.log(data)
         document.querySelector("h2").innerText = data.song
