a5b95933d2048ffa350f61380b4ae11f8dad9823
Ticia Francisco <ticiafrancisco@gmail.com>
2022-06-24 16:17:26 -0700

    Update api link to heroku

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", getBestSong)
 async function getBestSong(){
     const bandName = document.querySelector("input").value
     try{
-        const res = await fetch(`http://localhost:8000/api/${bandName}`)
+        const res = await fetch(`https://emo-band-songs-api.herokuapp.com/api/${bandName}`)
         const data = await res.json()
 
         console.log(data)
