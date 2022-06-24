b9123088a106e913240f68ad32c3107f047db67f
Ticia Francisco <ticiafrancisco@gmail.com>
2022-06-24 15:28:31 -0700

    Reformat bands data

--- server.js
+++ server.js
@@ -1,77 +1,76 @@
 const express = require("express")
-const app = express
+const app = express()
 const PORT = 8000
 const cors = require("cors")
 
 app.use(cors())
-app.use("static", express.static("./static/"))
+app.use("/static", express.static("./static/"))
 
 const bands = {
-    "unknown":
-    {
+    "unknown": {
         "song": "no data",
         "youtube": "#"
     },
-    "Dance Gavin Dance":
-	{
-		"song": "Alex English",
-		"youtube": "#"
-	},
-	"Paramore":
-	{
-		"song": "Pressure",
-		"youtube": "#"
-	},
-	"Panic! At The Disco":
-	{
-		"song": "Lying Is The Most Fun A Girl Can Have Without Taking Her Clothes Off",
-		"youtube": "#"
-	},
-	"Fall Out Boy":
-	{
-		"song": "Grand Theft Autumn",
-		"youtube": "#"
-	},
-	"My Chemical Romance":
-	{
-		"song": "I'm Not Okay (I Promise)",
-		"youtube": "#"
-	},
-	"Hawthorne Heights":
-	{
-		"song": "Ohio Is For Lovers",
-		"youtube": "#"
-	},
-	"Escape The Fate":
-	{
-		"song": "This War Is Ours (The Guillotine II)",
-		"youtube": "#"
-	},
-	"Mayday Parade":
-	{
-		"song": "Three Cheers For Five Years",
-		"youtube": "#"
-	},
-	"Jimmy Eat World":
-	{
-		"song": "Hear You Me",
-		"youtube": "#"
-	},
-	"Bright Eyes":
-	{
-		"song": "Lover I Don't Have To Love",
-		"youtube": "#"
-	}
+    "dance gavin dance": {
+        "bandName": "Dance Gavin Dance",
+        "song": "Alex English",
+        "youtube": "#"
+     },
+     "paramore": {
+        "bandName": "Paramore",
+        "song": "Pressure",
+        "youtube": "#"
+     },
+     "panic! at the disco": {
+        "bandName": "Panic! At The Disco",
+        "song": "Lying Is The Most Fun A Girl Can Have Without Taking Her Clothes Off",
+        "youtube": "#"
+     },
+     "fall out boy": {
+        "bandName": "Fall Out Boy",
+        "song": "Grand Theft Autumn",
+        "youtube": "#"
+     },
+     "my chemical romance": {
+        "bandName": "My Chemical Romance",
+        "song": "I'm Not Okay (I Promise)",
+        "youtube": "#"
+     },
+     "hawthorne heights": {
+        "bandName": "Hawthorne Heights",
+        "song": "Ohio Is For Lovers",
+        "youtube": "#"
+     },
+     "escape the fate": {
+        "bandName": "Escape The Fate",
+        "song": "This War Is Ours (The Guillotine II)",
+        "youtube": "#"
+     },
+     "mayday parade": {
+        "bandName": "Mayday Parade",
+        "song": "Three Cheers For Five Years",
+        "youtube": "#"
+     },
+     "jimmy eat world": {
+        "bandName": "Jimmy Eat World",
+        "song": "Hear You Me",
+        "youtube": "#"
+     },
+     "bright eyes": {
+        "bandName": "Bright Eyes",
+        "song": "Lover I Don't Have To Love",
+        "youtube": "#"
+     },
 }
 
 app.get('/', (req,res) =>{
-    res.sendFile(__dirname + "index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
-app.get('/api/:bandName', (req, res) =>{
-    const bandName = req.params.bandName.toLowerCase()
-    if(bands[bandName]){
-        res.json(bands[bandName])
+app.get('/api/:band', (req, res) =>{
+    const bandData = req.params.band.toLowerCase()
+    if(bands[bandData]){
+        res.json(bands[bandData])
     }else{
         res.json(bands['unknown'])
     }
